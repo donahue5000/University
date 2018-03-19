08e2ab6d157337eb69c72bf54443002632c3da50
Brian Donahue <donahue5000@gmail.com>
2018-03-18 19:36:08 -0700

    Create finalScore.java

---
+++ finalScore.java
@@ -0,0 +1,5 @@
+//Consider this example problem: You are given the quiz scores of a student. You are to compute the final quiz score, which 
+//is the sum of all scores after dropping the lowest one. For example, if the scores are
+//8  7  8.5  9.5  7  4  10
+//then the final score is 50.
+
